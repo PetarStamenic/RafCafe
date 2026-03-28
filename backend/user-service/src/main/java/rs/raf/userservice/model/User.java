5a41e35d0a569c15c4d82ffee29fa0e7586a9273
stiboly <lavandric04@gmail.com>
2026-03-28 16:35:48 +0100

    Refactor User entity annotation to separate @Entity and @Table definitions

--- backend/user-service/src/main/java/rs/raf/userservice/model/User.java
+++ backend/user-service/src/main/java/rs/raf/userservice/model/User.java
@@ -7,11 +7,13 @@ import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.Accessors;
 
-@Entity(name = "users")
+@Entity
+@Table(name = "users")
 @Getter
 @Setter
 @Accessors(chain = true)
