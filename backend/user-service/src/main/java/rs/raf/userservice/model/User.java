2f980bf58442b9c2a9356a7612dfeeac26546a9b
stiboly <lavandric04@gmail.com>
2026-03-30 18:08:59 +0200

    Change id field type from primitive long to Long in User entity

--- backend/user-service/src/main/java/rs/raf/userservice/model/User.java
+++ backend/user-service/src/main/java/rs/raf/userservice/model/User.java
@@ -20,7 +20,7 @@ import lombok.experimental.Accessors;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
     @Column(nullable = false, unique = true)
     private String username;
