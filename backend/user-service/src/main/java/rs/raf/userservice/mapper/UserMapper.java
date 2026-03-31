311c416aeb768eaf29def136f0b1fed0c36a28b0
stiboly <lavandric04@gmail.com>
2026-03-31 12:52:14 +0200

    Add user registration and response DTOs with mapping logic

---
+++ backend/user-service/src/main/java/rs/raf/userservice/mapper/UserMapper.java
@@ -0,0 +1,31 @@
+package rs.raf.userservice.mapper;
+
+import org.springframework.stereotype.Component;
+
+import rs.raf.userservice.dto.user.RegisterUserRequestDTO;
+import rs.raf.userservice.dto.user.RegisterUserResponseDTO;
+import rs.raf.userservice.dto.user.UserResponseDTO;
+import rs.raf.userservice.model.User;
+
+@Component
+public class UserMapper {
+    public User toEntity(RegisterUserRequestDTO dto) {
+        return new User()
+            .setUsername(dto.username())
+            .setPassword(dto.password())
+            .setEmail(dto.email())
+            .setFirstName(dto.firstName())
+            .setLastName(dto.lastName());
+    }
+
+    public UserResponseDTO toDTO(User user) {
+        return new UserResponseDTO(
+            user.getId(),
+            user.getUsername(),
+            user.getEmail(),
+            user.getFirstName(),
+            user.getLastName(),
+            user.getRole().name()
+        );
+    }
+}
